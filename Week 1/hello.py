9ed014d5f029e85032f2a57581bb3f92e0100d9a
Rafał Borowski <rafal@borowski.design>
2024-01-03 21:00:55 +0100

    Merging and cleaning code

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -1,8 +1,5 @@
-# Ask user for their name
-name = input("What's your name? ")
-
-#Remove whitespace from str and capitalize it
-name = name.strip().title()
+# Ask user for their name, remove whitespace from str and capitalize it
+name = input("What's your name? ").strip().title()
 
 # Say hello to user
 print(f"Hello, {name}")
