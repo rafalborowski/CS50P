1d3e76dbf26b0d929279c11ef77c6c1a9fa04590
Rafał Borowski <rafal@borowski.design>
2024-01-03 21:06:47 +0100

    Adding split method

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -1,5 +1,8 @@
 # Ask user for their name, remove whitespace from str and capitalize it
 name = input("What's your name? ").strip().title()
 
+# Split user's name into first name and last name
+first, last = name.split(" ")
+
 # Say hello to user
-print(f"Hello, {name}")
+print(f"Hello, {first}")
