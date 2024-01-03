df04e5b55a608d513186f941d19afd92a1c596d0
Rafał Borowski <rafal@borowski.design>
2024-01-03 20:55:40 +0100

    Formatting string methods

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -1,5 +1,8 @@
 # Ask user for their name
 name = input("What's your name? ")
 
+#Remove whitespace from string
+name = name.strip()
+
 # Say hello to user
-print("Hello,", name)
+print(f"Hello, {name}")
